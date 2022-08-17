3b77e8dc128b9ef1c02b6cc936b423087eed76ad
Sergio <goich1991@gmail.com>
2022-08-17 14:21:48 +0300

    Complete

---
+++ Homework5_2/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+Console.WriteLine("Напиши количество чисел в массиве: ");
+int size = Convert.ToInt32(Console.ReadLine());
+
+int[] array = new int[size];
+for (int i = 0; i < size; i++)
+    array[i] = new Random().Next(-10, 100);
+Console.WriteLine("[" + string.Join(", ", array) + "]");
+
+int j = 1;
+int sum = 0;
+
+while (j < size)
+{
+    sum = sum + array[j];
+    j = (j + 2);
+}
+Console.WriteLine("Сумма чисел на нечётных позициях = " + (sum));
---
+++ Homework5_3/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 38: Задайте массив вещественных чисел. 
+// Найдите разницу между максимальным и минимальным элементов массива.
+// [3 7 22 2 78] -> 76
+
+Console.WriteLine("Напиши количество чисел в массиве: ");
+int size = Convert.ToInt32(Console.ReadLine());
+
+int[] array = new int[size];
+int index = 0;
+
+Console.WriteLine("Введи числа в массив: ");
+
+while (index < size)
+{
+    array[index] = Convert.ToInt32(Console.ReadLine());
+    index++;
+}
+Console.WriteLine("[" + string.Join(", ", array) + "]");
+
+if (index != 0)
+    index = 0;
+int max_index = 0;
+int min_index = 0;
+
+while (index < size)
+{
+    if (array[min_index] > array[index])
+        min_index = index;
+    if (array[max_index] < array[index])
+        max_index = index;
+    index++;
+}
+Console.WriteLine(array[min_index]);
+Console.WriteLine(array[max_index]);
+
+Console.WriteLine("Разница между максимальным и минимальным элементом массива = " + (array[max_index] - array[min_index]));
