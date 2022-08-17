1f90a475605e99abda72a451f1a4d66629188ceb
Sergio <goich1991@gmail.com>
2022-08-17 18:57:20 +0300

    complete with edit

--- Homework5_1/Program.cs
+++ Homework5_1/Program.cs
@@ -2,13 +2,13 @@
 // Напишите программу, которая покажет количество чётных чисел в массиве.
 //[345, 897, 568, 234] -> 2
 
-int[] array = new int[4];
-int sum = 0;
-for (int i = 0; i < 4; i++)
+int[] array = new int[10];
+int count = 0;
+for (int i = 0; i < 10; i++)
 {
     array[i] = new Random().Next(100,999);
     if(array[i] % 2 == 0)
-        sum = sum + array[i];
+        count++;
 }
 Console.WriteLine("[" + string.Join(", ", array) + "]");
-Console.WriteLine("Сумма четных элементов = " + (sum));
+Console.WriteLine("Кол-во четных элементов = " + (count));
--- Homework5_3/Program.cs
+++ Homework5_3/Program.cs
@@ -1,36 +1,54 @@
-﻿// Задача 38: Задайте массив вещественных чисел. 
-// Найдите разницу между максимальным и минимальным элементов массива.
-// [3 7 22 2 78] -> 76
+﻿// // Задача 38: Задайте массив вещественных чисел. 
+// // Найдите разницу между максимальным и минимальным элементов массива.
+// // [3 7 22 2 78] -> 76
 
-Console.WriteLine("Напиши количество чисел в массиве: ");
-int size = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Напиши количество чисел в массиве: ");
+// int size = Convert.ToInt32(Console.ReadLine());
 
-int[] array = new int[size];
-int index = 0;
+// int[] array = new int[size];
+// int index = 0;
 
-Console.WriteLine("Введи числа в массив: ");
+// Console.WriteLine("Введи числа в массив: ");
 
-while (index < size)
+// while (index < size)
+// {
+//     array[index] = Convert.ToInt32(Console.ReadLine());
+//     index++;
+// }
+// Console.WriteLine("[" + string.Join(", ", array) + "]");
+
+// if (index != 0)
+//     index = 0;
+// int max_index = 0;
+// int min_index = 0;
+
+// while (index < size)
+// {
+//     if (array[min_index] > array[index])
+//         min_index = index;
+//     if (array[max_index] < array[index])
+//         max_index = index;
+//     index++;
+// }
+// Console.WriteLine(array[min_index]);
+// Console.WriteLine(array[max_index]);
+
+// Console.WriteLine("Разница между максимальным и минимальным элементом массива = " + (array[max_index] - array[min_index]));
+
+double[] array = new double[10];
+double x;
+for (int i = 0; i < array.Length; i++)
 {
-    array[index] = Convert.ToInt32(Console.ReadLine());
-    index++;
+    x = new Random().NextDouble() * 100;
+    array[i] = Math.Round(x, 4);
 }
 Console.WriteLine("[" + string.Join(", ", array) + "]");
-
-if (index != 0)
-    index = 0;
-int max_index = 0;
-int min_index = 0;
-
-while (index < size)
-{
-    if (array[min_index] > array[index])
-        min_index = index;
-    if (array[max_index] < array[index])
-        max_index = index;
-    index++;
+double max_array = array[0], min_array = array[0];
+for (int i = 1; i < array.Length; i++)
+    {
+    if (array[i] > max_array)
+        max_array = array[i];
+    if (array[i] < min_array)
+        min_array = array[i];
 }
-Console.WriteLine(array[min_index]);
-Console.WriteLine(array[max_index]);
-
-Console.WriteLine("Разница между максимальным и минимальным элементом массива = " + (array[max_index] - array[min_index]));
+Console.WriteLine(max_array - min_array);
